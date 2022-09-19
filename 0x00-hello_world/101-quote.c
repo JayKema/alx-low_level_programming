b69d891c3b5f6289a6425265be16efd4f9819ec4
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-18 21:50:22 -0700

    Add: print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stderr.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints specified text
+ * Return: 1 always on Sucess
+ */
+int main(void)
+{
+	char mystr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	putchar(mystr);
+	return (1);
+}
