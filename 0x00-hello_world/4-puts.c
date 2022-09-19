9ed229d5e796c7905a6719dfaa3a04f66532e0fd
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-18 20:16:44 -0700

    Add: use puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Sucess)
+ */
+int main(void)
+{
+	char mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(mystr);
+	return (0);
+}
