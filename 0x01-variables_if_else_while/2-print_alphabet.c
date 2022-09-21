657c5ec203371ddcd3972b81c37cad31654b8ac5
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-21 12:23:34 -0700

    Add: script to print lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints alphabets in lower case
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
