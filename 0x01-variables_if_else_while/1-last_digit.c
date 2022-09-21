5a709a379e276f98faa90a03b0fa27c957836ae0
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-21 10:59:14 -0700

    Add: prints category of last digit in number n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * last_digit - prints the category of the last digit
+ * @n: integer n
+ *
+ * Description: @TODO
+ * Return: none
+ */
+
+/* betty style doc for function main goes there */
+
+void last_digit(int n)
+{
+	int digit, last;
+
+	digit = n;
+	last = n % 10;
+	if (last == 0)
+	{
+		printf("Last digit of %d is 0 and is %d\n", digit, last);
+	}
+	else
+	{
+		if (last > 5)
+		{
+			printf("Last digit of %d is %d and is greater than 5", digit, last);
+	}
+		else if ((last < 6) && (last != 0))
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0", digit, last);
+	}
+	}
+}
+
+/**
+ * main - code entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	last_digit(n = n);
+	return (0);
+}
