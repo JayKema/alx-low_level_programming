62ca931d2e5b7f0619dba048ba653bd7ed47405f
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-21 14:09:38 -0700

    Add: script to print base16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints single digit numbers
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 48;
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+	ch = 97;
+	while (ch < 102)
+	{
+		putchar(ch);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
