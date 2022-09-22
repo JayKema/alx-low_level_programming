1b4fb214ecf496437ccf833d430fd214b3452ea6
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-22 09:10:09 -0700

    Add: scripts to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * print_putchar - prints putchar to standard output
+ *
+ * Description: prints _putchar to standar output
+ * Return: 0 if sucessful
+ */
+int print_putchar(void)
+{
+	char txt[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(txt[i]);
+	}
+	putchar(10);
+	return (0);
+}
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar to standar output
+ * Return: 0 if sucessful
+ */
+void main(void)
+{
+	print_putchar();
+}
