f9713150a947518629904b34aa82c66dc124315f
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-22 15:03:18 -0700

    Create: islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower - check if an alphabet is lower case
+ * @c: letter input
+ *
+ * Description: check for the case of an alphabet
+ * Return: 1 if c is a lowercase; otherwise 0
+ */
+int _islower(int c)
+{
+	int val;
+
+	val = _putchar(c);
+	if (val >= 97 && val <= 122)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
