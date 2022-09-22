5390a39aa3b9e7b32349d5cffbfb0ff6876b1269
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-21 19:11:04 -0700

    Add: print four-digits single digit combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints four-digits combinations with replacement
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int ch, pos2, pos3, pos4;
+
+	ch = 48;
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (pos2 = 48; pos2 <= 57; pos2++)
+		{
+			for (pos3 = 48; pos3 <= 57; pos3++)
+			{
+				for (pos4 = pos2 + 1; pos4 <= 57; pos4++)
+				{
+					putchar(ch);
+					putchar(pos2);
+					putchar(32);
+					putchar(pos3);
+					putchar(pos4);
+					if (!(((ch == 57) && (pos2 == 56)) && ((pos3 == 57) && (pos4 == 57))))
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
