0cb1de368b3f6029a2477918651e84da186a98e9
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-23 16:38:20 -0700

    Create: 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * fibonacci - prints fibonacci numbers
+ * @n:number of terms
+ *
+ * Return: Always greater than 0
+ */
+int fibonacci(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n == 2)
+		return (2);
+	else
+		return ((fibonacci(n - 1) + fibonacci(n - 2)));
+}
+/**
+ * print_fib_terms - print fibonacci terms
+ * @n: int length of fibonacci sequence
+ *
+ * Return: prints comma delimited fib sequence
+ */
+void print_fib_terms(int n)
+{
+	int val, term;
+
+	for (term = 1; term <= n; term++)
+	{
+		val = fibonacci(term);
+		if (term == n)
+			printf("%d\n", val);
+		else
+			printf("%d%s%s", val, ",", " ");
+	}
+}
+/**
+ * main - check code
+ *
+ * Return: none
+ */
+void main(void)
+{
+	print_fib_terms(50);
+}
