ecd549770d172703864db676dfb3756899778149
Jeremiah Nwosu <39865899+JayKema@users.noreply.github.com>
2022-09-25 02:29:19 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,25 +8,9 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-        /*int i;
-
-        printf("Infinite loop incoming :(\n");
+	int i;
 
         i = 0;
-	
-	while (i < 10)
-        {
-                putchar(i);
-        }*/
-
-        printf("Infinite loop avoided! \\o/\n");
-
-=======
-        int i;
-        
-        i = 0;
-        positive_or_negative(i);
->>>>>>> 13cd18eb3e26bebfeb4dde63db99603f0b000ebf
+        positive_or_negative(i);	
         return (0);
 }
