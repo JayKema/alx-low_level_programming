0c48306c62f84a67bb7172941d4063bbf62cb6d2
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2022-09-25 21:17:25 -0700

    Fix: function documentation

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - swp the value of two pointers a and b
+ * swap_int - swp the value of two pointers a and b
  * @a: pointer to an integer
  * @b: pointer to an integer
  *
