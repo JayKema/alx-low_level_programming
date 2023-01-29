39199bdd6393f0130a92e12d45eaa3dbbb16e416
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2023-01-29 07:43:31 -0800

    fix code to print number

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - converts characters to integers
+ * @s: pointer to the string
+ *
+ * Return:digit of arbitrary length in a string or EOF if error
+ */
+int _atoi(char *s)
+{
+	int num = 0;
+	int i = 0;
+	char temp;
+	int sign = 1;
+
+	while (s[i] != '\0')
+	{
+		temp = s[i];
+		if (temp >= 48 && temp <= 57)
+		{
+			if ((sign == 1) && (s[i - 1] == '-'))
+				sign = -1;
+			if (num > 0)
+				num  =  (num * 10) + (int)(temp - '0');
+			else
+				num  = num + (int)(temp - '0');
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+		i++;
+	}
+	if (num != 0)
+		return (sign * num);
+	else
+		return (0);
+}
