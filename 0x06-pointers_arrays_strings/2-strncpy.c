a556b73861a8ddabbc4e6d6f7bac9735c6219e27
Jeremiah Nwosu <jeramiahfelix.n@gmail.com>
2023-01-31 13:53:56 -0800

    Initial commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strlen- determine the length of a string
+ * @str: pointer to input string
+ *
+ * Return: Int > 1 if string input is not NuLL. Otherwise, 0
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * _strncpy- copy and arbitrary amount of char from str to another
+ * @src: pointer to source string
+ * @dest: pointer to distination string
+ * @n: max number of char to append to dest str
+ *
+ * Description: Append and arbitrary length of charaters to the dest string
+ * Return: concatenated string if string is not NULL, otherwhisse EOF
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int dest_len = _strlen(dest);
+	int i;
+
+	for (i = 0; i < n &&  src[i] != '\0'; i++)
+	{
+		if (i > dest_len - 1)
+			break;
+		dest[i] = src[i];
+	}
+	return (dest);
+}
