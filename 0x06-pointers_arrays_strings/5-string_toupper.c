03228947c4aec5d375ee9ec6c8f34ec2df556a2f
Jeremiah Nwosu <jeremiahfelix.n@gmail.com>
2023-05-17 17:30:35 -0700

    Add 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#define LOW_UP_BOUND 122
+#define LOW_LOW_BOUND 97
+/**
+ * string_toupper: transforms lowercase to uppercase
+ * @str: pointer to string
+ *
+ * Return: modified char
+ */
+char *string_toupper(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if(str[i] >= LOW_LOW_BOUND && str[i] <= LOW_UP_BOUND)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
